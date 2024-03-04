ebda2b05a611e4d008255d9170f940e5dd2274cd
shyam <rajshyam151@gmail.com>
2024-03-04 15:50:22 +0530

    component created

---
+++ food.jsx
@@ -0,0 +1,5 @@
+function Fav_food(){
+    return <h1> Chicken Biriyani</h1>
+}
+
+export default Fav_food
---
+++ greeting.jsx
@@ -0,0 +1,4 @@
+function Greeting(){
+    return <h1> Hello world</h1>
+}
+ export default Greeting
